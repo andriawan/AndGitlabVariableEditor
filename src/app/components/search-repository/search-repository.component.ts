c355e52322401d461f38ddd58885640d64d07a1f
Muhammad Irwan Andriawan <irwan@pmg.id>
2021-12-27 21:40:30 +0700

    improvement 7

--- src/app/components/search-repository/search-repository.component.ts
+++ src/app/components/search-repository/search-repository.component.ts
@@ -1,5 +1,5 @@
 import { HttpErrorResponse } from '@angular/common/http';
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
 import { ToggleGitlabValue } from 'src/app/enum/toggle-gitlab-value';
 import { GitlabProject } from 'src/app/interfaces/gitlab-project';
 import { GitlabVar } from 'src/app/interfaces/gitlab-var';
@@ -27,6 +27,7 @@ export class SearchRepositoryComponent implements OnInit {
     web_url: "",
   }
   inputValueVisibility: ToggleGitlabValue = ToggleGitlabValue.PASSWORD;
+  @ViewChild("file_json") input: ElementRef | undefined;
 
   constructor(
     private gitlabVariableService: GitlabVariableService,
@@ -39,6 +40,44 @@ export class SearchRepositoryComponent implements OnInit {
     })
   }
 
+  importJSON() {
+    this.input?.nativeElement.click();
+  }
+
+  processJSON(event: Event) {
+    const element = event.currentTarget as HTMLInputElement;
+    const fileReader = new FileReader();
+    let fileList: FileList | null = element.files;
+    let file: File | null | undefined = fileList?.item(0);
+    if (file) {
+      fileReader.onloadend = (e) => {
+        this.validateJSONData(e.target?.result)
+      }
+      fileReader.readAsText(file);
+    }
+  }
+
+  validateJSONData(json: FileReader["result"] | undefined) {
+    try {
+      let data: any[] = JSON.parse(json as string);
+      if(!Array.isArray(data)) throw Error(`not valid array json.`)
+      let processedData = data.filter(item => {
+        return item.key && item.value
+      }).map((item) => {
+        return {
+          key: item.key,
+          value: item.value
+        }
+      })
+      if (processedData.length <= 0) { 
+        throw Error(`not valid json. please provide object like {"key" : "sample", "value": "sample"}`)
+      }
+      console.log(processedData)
+    } catch (error) {
+      window.alert(error);
+    }
+  }
+
   exportVariable(): void {
     if (this.gitlabVarData.length === 0) return;
     let data = this.gitlabVarData.map(value => { 
@@ -47,7 +86,6 @@ export class SearchRepositoryComponent implements OnInit {
 
     let myBlob = new Blob([data], { type: "application/octet-stream" });
     let blobURL = URL.createObjectURL(myBlob);
-    console.log(blobURL);
     let href = document.createElement("a");
     href.href = blobURL;
     href.download = `env-${this.repoDetail.name}`;
@@ -65,7 +103,6 @@ export class SearchRepositoryComponent implements OnInit {
 
     let myBlob = new Blob([JSON.stringify(data)], { type: "application/json" });
     let blobURL = URL.createObjectURL(myBlob);
-    console.log(blobURL);
     let href = document.createElement("a");
     href.href = blobURL;
     href.download = `data-var-${this.repoDetail.name}`;
